e37e9496e2281feab791e406e2edff6bb5e3d82d
Mir Yasin Ali <miryasin.miki@gmail.com>
2024-04-27 06:45:07 +0000

    Pong: Nuke system core from vendorsetup.sh
    * Also Update it

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,41 +3,34 @@ echo 'Starting vendorsetup processes needed for your device'
 # Stuffs to rm -rf
 rm -rf hardware/qcom-caf/sm8450/display
 rm -rf device/qcom/sepolicy_vndr/sm8450
-rm -rf system/core
 rm -rf vendor/qcom/opensource/vibrator
 
-echo 'Cloning Vendor tree [1/6]'
-# Vendor Tree
+echo 'Cloning Vendor tree [1/5]'
+# vendor/nothing/Pong
 git clone https://gitlab.com/RealYasin/vendor_nothing_pong.git vendor/nothing/Pong
 
 echo 'DONE'
 
-echo 'Cloning Kernel tree [2/6]'
-# Kernel Tree
+echo 'Cloning Kernel tree [2/5]'
+# kernel/nothing/sm8475
 git clone --depth=1 https://github.com/Nothing-phone-2-Development/android_kernel_nothing_sm8475.git kernel/nothing/sm8475
 
 echo 'DONE'
 
-echo 'Cloning hardware/qcom-caf/sm8450/display [3/6]'
-# Display Hal
+echo 'Cloning Display Hal [3/5]'
+# hardware/qcom-caf/sm8450/display
 git clone --depth=1 https://github.com/Nothing-phone-2-Development/android_hardware_qcom_display.git hardware/qcom-caf/sm8450/display
 
 echo 'DONE'
 
-echo 'Picking commit in device/qcom/sm8450/sepolicy_vndr [4/6]'
-# Sepolicy_vndr
-git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr.git -b lineage-21.0-caf-sm8450 device/qcom/sepolicy_vndr/sm8450 && cd device/qcom/sepolicy_vndr/sm8450 && git fetch https://github.com/Nothing-phone-2-Development/android_device_qcom_sepolicy_vndr.git && git cherry-pick d9fa2f5f3c36dae723d9ca0899dc0ab85e149e9d && cd ../../../..
+echo 'Cloning sepolicy_vndr [4/5]'
+# device/qcom/sepolicy_vndr/sm8450
+git clone https://github.com/PongxViolet/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8450
 
 echo 'DONE'
 
-echo 'Reverting commits in system core [5/6]'
-# system/core
-git clone https://github.com/Project-Elixir/android_system_core.git system/core && cd system/core && git revert 9f5ec999c824840279c67ddf3e902b387ccc482f && git revert 51f077f61f9aa6e595be55ed094414f87479de68 && cd ../..
-
-echo 'DONE'
-
-echo 'Cloning oss Vibrator [6/6]'
+echo 'Cloning oss Vibrator [5/5]'
 # vendor/qcom/opensource/vibrator
-git clone --depth=1 https://github.com/PixelExperience/vendor_qcom_opensource_vibrator -b thirteen  vendor/qcom/opensource/vibrator
+git clone --depth=1 https://github.com/PongxViolet/vendor_qcom_opensource_vibrator.git  -b thirteen vendor/qcom/opensource/vibrator
 
-echo 'Done and Completed'
+echo 'DONE'
