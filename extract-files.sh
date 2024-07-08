8e3d62db62ba0b8a1347f9cde9b9eff5e3a9d90b
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    Pong: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,7 +71,7 @@ twt_congestion_timeout=0
             }'  "${2}"
             ;;
         vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
-            sed -Ei "/media_codecs_(google_audio|google_telephony|google_video|vendor_audio)/d" "${2}"
+            sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
             ;;
     esac
 }
