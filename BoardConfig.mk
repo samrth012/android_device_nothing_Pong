50ebef50fde8d3b521b6fd7bdf8c1cb7697c770b
Samrth Singh <samrth012@gmail.com>
2024-08-01 06:14:43 +0200

    Pong: Build without ksu

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 
 TARGET_KERNEL_SOURCE := kernel/nothing/sm8475
-TARGET_KERNEL_CONFIG := vendor/meteoric_defconfig ksu.config
+TARGET_KERNEL_CONFIG := vendor/meteoric_defconfig 
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
