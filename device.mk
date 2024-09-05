4ee4e91e5a42d1deed73f1dd36f90468acdd9be6
Abhay Singh Gill <abhaygill017@gmail.com>
2024-09-05 23:36:46 +0530

    Pong: Add nothing charging control service
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- device.mk
+++ device.mk
@@ -132,6 +132,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.7.vendor \
+    libcamera2ndk_vendor \
+    vendor.qti.hardware.camera.aon@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
+# Charging
+PRODUCT_PACKAGES += \
+    nt-charging-control
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
