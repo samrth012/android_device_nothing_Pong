d5a56e042c8a7b884fd0fc8e997dcf09afa3b111
Bruno Martins <bgcngm@gmail.com>
2024-11-12 15:11:25 +0530

    Pong: Address seccomp filter failures
    Fixes:
     W ATFWD-daemon: libminijail[4334]: logging seccomp filter failures
     E ATFWD-daemon: libminijail[4334]: blocked syscall: gettid
     I init    : Service 'vendor.atfwd' (pid 4334) received signal 31
     I init    : Sending signal 9 to service 'vendor.atfwd' (pid 4334) process group...
     ...
     W wfdhdcphalservice: libminijail[5312]: logging seccomp filter failures
     E wfdhdcphalservice: libminijail[5312]: blocked syscall: gettid
     I init    : Service 'wfdhdcphalservice' (pid 5312) received signal 31
     I init    : Sending signal 9 to service 'wfdhdcphalservice' (pid 5312) process group...
    
    Change-Id: Ic259464520db6e77f06a3d0820a63662aa91659c
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -66,6 +66,10 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('android.hardware.security.rkp-V1-ndk.so'),
     ('vendor/etc/media_codecs_cape.xml', 'vendor/etc/media_codecs_cape_vendor.xml'): blob_fixup()
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
+    ('vendor/etc/seccomp_policy/atfwd@2.0.policy', 'vendor/etc/seccomp_policy/wfdhdcphalservice.policy'): blob_fixup()
+        .add_line_if_missing('gettid: 1'),
+    'vendor/etc/seccomp_policy/qwesd@2.0.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
     'vendor/lib64/libcamximageformatutils.so': blob_fixup()
         .replace_needed('vendor.qti.hardware.display.config-V2-ndk_platform.so', 'vendor.qti.hardware.display.config-V2-ndk.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
