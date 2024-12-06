227ecec747f251b750ce9db8e57dee917efc65e1
Samrth Singh <samrth012@gmail.com>
2024-12-06 20:11:38 +0100

    Update Vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ rm -rf packages/apps/ParanoidGlyph
 
 echo 'Cloning Vendor tree [1/6]'
 # vendor/nothing/Pong
-git lfs clone https://gitlab.com/samrth012/android_vendor_nothing_Pong.git -b lineage-21 vendor/nothing/Pong
+git lfs clone https://github.com/samrth012/vendor_nothing_Pong.git -b 15 vendor/nothing/Pong
 
 echo 'DONE'
 
@@ -18,19 +18,19 @@ echo 'DONE'
 
 echo 'Cloning Display Hal [3/6]'
 # hardware/qcom-caf/sm8450/display
-git clone https://github.com/Nothing-phone-2-Development/android_hardware_qcom_display.git hardware/qcom-caf/sm8450/display
+git clone https://github.com/Pong-Development/hardware_qcom-caf_sm8450_display.git hardware/qcom-caf/sm8450/display
 
 echo 'DONE'
 
 echo 'Cloning PAGlyph Repo [4/6]'
 # packages/apps/ParanoidGlyph
-git clone https://github.com/ProjectGhostOS/android_packages_apps_ParanoidGlyph.git packages/apps/ParanoidGlyph
+git clone https://github.com/Pong-Development/packages_apps_ParanoidGlyph.git packages/apps/ParanoidGlyph
 
 echo 'DONE'
 
 echo 'Cloning Dolby [5/6]'
 # hardware/dolby
-git clone https://github.com/FlamingoOS-Devices/hardware_dolby.git hardware/dolby
+git clone https://github.com/Pong-Development/hardware_dolby.git hardware/dolby
 
 echo 'Pick Sepolicy Commit [6/6]'
 # device/qcom/sepolicy_vndr
