9d77553aa3641f8c6a21ef85a95c6da85ff85ea7
Samrth Singh <samrth012@gmail.com>
2025-01-02 16:27:47 +0530

    Move dolby to dt

--- device.mk
+++ device.mk
@@ -149,7 +149,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
+$(call inherit-product, device/nothing/Pong/dolby/dolby.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
