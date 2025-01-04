Neoteric-OS/device_nothing_Pong

Pong: Implement vendor.lineage.powershare@1.0 HAL
Change-Id: I8022ac47411369f3ede969417fd46e08fb984e24

device.mk
@@ -311,6 +311,10 @@ TARGET_BOARD_PLATFORM := taro
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.nothing
+
 # QTI Components
 TARGET_COMMON_QTI_COMPONENTS := \
     alarm \

hidl/powershare/PowerShare.cpp
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.powershare@1.0-service.nothing"
+
+#include "PowerShare.h"
+
+#include <android-base/file.h>
+
+#include <fstream>
+
+#define WIRELESS_TX_PATH "/sys/class/qcom-battery/wireless_boost_en"
+#define WIRELESS_TX_ON 1
+#define WIRELESS_TX_OFF 0
+
+namespace {
+
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+    file >> result;
+    return file.fail() ? def : result;
+}
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+}  // anonymous namespace
+
+namespace vendor {
+namespace lineage {
+namespace powershare {
+namespace V1_0 {
+namespace implementation {
+
+Return<bool> PowerShare::isEnabled() {
+    return get(WIRELESS_TX_PATH, WIRELESS_TX_OFF) != WIRELESS_TX_OFF;
+}
+
+Return<bool> PowerShare::setEnabled(bool enable) {
+    set(WIRELESS_TX_PATH, enable ? WIRELESS_TX_ON : WIRELESS_TX_OFF);
+    return isEnabled();
+}
+
+Return<uint32_t> PowerShare::getMinBattery() {
+    return 0;
+}
+
+Return<uint32_t> PowerShare::setMinBattery(uint32_t /*minBattery*/) {
+    return getMinBattery();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace powershare
+}  // namespace lineage
+}  // namespace vendor

hidl/powershare/service.cpp
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.powershare@1.0-service.nothing"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "PowerShare.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+using android::sp;
+
+using vendor::lineage::powershare::V1_0::IPowerShare;
+using vendor::lineage::powershare::V1_0::implementation::PowerShare;
+
+int main() {
+    sp<IPowerShare> powerShareService = new PowerShare();
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (powerShareService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Can't register PowerShare HAL service";
+        return 1;
+    }
+
+    joinRpcThreadpool();
+
+    return 0; // should never get here
+}

samrth012/android_device_nothing_Pong

Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

device.mk
@@ -378,6 +378,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.nt
+
 # QMI
 PRODUCT_PACKAGES += \
     libcurl.vendor \

powershare/PowerShare.cpp
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.powershare@1.0-service.nt"
+
+#include "PowerShare.h"
+
+#include <android-base/file.h>
+
+#include <fstream>
+
+#define WIRELESS_TX_PATH "/sys/class/qcom-battery/wireless_boost_en"
+#define WIRELESS_TX_ON 1
+#define WIRELESS_TX_OFF 0
+
+namespace {
+
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+    file >> result;
+    return file.fail() ? def : result;
+}
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+}  // anonymous namespace
+
+namespace vendor {
+namespace lineage {
+namespace powershare {
+namespace V1_0 {
+namespace implementation {
+
+Return<bool> PowerShare::isEnabled() {
+    return get(WIRELESS_TX_PATH, WIRELESS_TX_OFF) != WIRELESS_TX_OFF;
+}
+
+Return<bool> PowerShare::setEnabled(bool enable) {
+    set(WIRELESS_TX_PATH, enable ? WIRELESS_TX_ON : WIRELESS_TX_OFF);
+    return isEnabled();
+}
+
+Return<uint32_t> PowerShare::getMinBattery() {
+    return 0;
+}
+
+Return<uint32_t> PowerShare::setMinBattery(uint32_t /*minBattery*/) {
+    return getMinBattery();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace powershare
+}  // namespace lineage
+}  // namespace vendor

powershare/service.cpp
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.powershare@1.0-service.nt"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "PowerShare.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+using android::sp;
+
+using vendor::lineage::powershare::V1_0::IPowerShare;
+using vendor::lineage::powershare::V1_0::implementation::PowerShare;
+
+int main() {
+    sp<IPowerShare> powerShareService = new PowerShare();
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (powerShareService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Can't register PowerShare HAL service";
+        return 1;
+    }
+
+    joinRpcThreadpool();
+
+    return 0; // should never get here
+}